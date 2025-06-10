fbaf81097de0bc9261ce3614524e3a62fa8124bc
Alex <alexbirdev@gmail.com>
2025-06-10 05:15:36 +0300

    проблема с картинками11

--- backend/memory_book/settings.py
+++ backend/memory_book/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = ':8100/media/'
+MEDIA_URL = 'http://memory-book.bybirukov.ru:8100/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Для production добавьте:
